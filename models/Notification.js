43bea0ace8f00807427c6db2ca93f7d89ae54cb8
kienbuiii <kien36652@gmail.com>
2024-10-31 14:53:52 +0700

    fix model

---
+++ models/Notification.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const notificationSchema = new mongoose.Schema({
+    recipient: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    sender: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    type: {
+        type: String,
+        required: true,
+        enum: ['like', 'comment', 'follow', 'mention'] // các loại thông báo
+    },
+    post: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Post'
+    },
+    read: {
+        type: Boolean,
+        default: false
+    },
+    content: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('Notification', notificationSchema);
